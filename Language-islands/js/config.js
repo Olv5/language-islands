f887ffc788d337572d416988e556a5cfbfdc73f9
Olv5 <Olavios153@gmail.com>
2026-06-07 00:19:53 +0100

    Delete Language-islands/js/config.js

--- Language-islands/js/config.js
+++
@@ -1,5 +0,0 @@
-// Substitui pelos teus valores copiados do Supabase
-const supabase = window.supabase.createClient(
-  'https://flsncrnxgxmfkbcfedwl.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZsc25jcm54Z3htZmtiY2ZlZHdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA3NTc1NjAsImV4cCI6MjA5NjMzMzU2MH0.VTt-DPjvb9ixWvXQWdn5XezxyjG0Pf5TH7iUD-TJ0AQ'
-);
